6e1b0ddb7af166f9dac2c6b6fe901745e6d953cd
poojalandejosh <pooja.lande@joshsoftware.com>
2026-06-11 11:35:34 +0530

    Implement assignment-05 with customer management features, including authentication, CRUD operations, and database integration using Sequelize. Update package.json and package-lock.json to include new dependencies.

---
+++ src/assignment-05/index.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import sequelize from "./config/db.ts";
+import "./models/Customer.model.ts";
+import customerRoutes from "./routes/customer.routes";
+import authRoutes from "./routes/auth.routes";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/auth',authRoutes);
+
+app.use('/customer',customerRoutes);
+
+const startServer = async ()=>{
+    try{
+        await sequelize.authenticate();
+        console.log("Database connected successfully");
+        await sequelize.sync();
+        console.log("Database tables synced");
+        app.listen(process.env.PORT,()=>{
+            console.log(`Server is running on port  dndndj ${process.env.PORT}`);
+        });
+    }catch(error){
+        console.error("Failed to start server:", error);
+        process.exit(1);
+    }
+}
+
+startServer();
---
+++ src/assignment-05/middleware/auth.middleware.ts
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken";
+import type { Request, Response, NextFunction } from "express";
+
+export const authMiddleware=(
+    req:Request,
+    res:Response,
+    next:NextFunction
+)=>{
+   try{
+   const authHeader = req.headers.authorization;
+
+   if(!authHeader || !authHeader.startsWith("Bearer ")){
+    return res.status(401).json({message:"Unauthorized"});
+   }
+
+   const token = authHeader.split(" ")[1];
+
+   if(!token){
+    return res.status(401).json({message:"Auth token is missing"});
+   }
+   
+   const decoded = jwt.verify(token,process.env.JWT_SECRET!) as {id:number};
+
+   (req as any).user = decoded;
+
+   next();
+   
+   }catch(error){
+    return res.status(401).json({message:"Unauthorized"});
+   }
+}
