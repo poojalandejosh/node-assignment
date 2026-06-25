89b3d8956a941e7b1610729a12c8ad82893668a8
poojalandejosh <pooja.lande@joshsoftware.com>
2026-06-25 14:36:45 +0530

    Enhance root endpoint response to include HTTP status and method information

--- src/assignment-03/index.ts
+++ src/assignment-03/index.ts
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/',(req,res)=>{
-    res.send({message: "Assignment 3: CORS + MySQL API"});
+    res.status(200).json({message: "Assignment 3: CORS + MySQL API",method:"GET"});
 });
 
 
