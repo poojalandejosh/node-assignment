bf33f5e41faeb40770dd897e3041a456c6aeee7c
poojalandejosh <pooja.lande@joshsoftware.com>
2026-06-25 13:27:04 +0530

    Refactor index.ts for improved argument parsing and validation; ensure port is within valid range

--- src/assignment-02/index.ts
+++ src/assignment-02/index.ts
@@ -1,27 +1,31 @@
-import http from 'http';
+import http from "http";
 
 const args = process.argv.slice(2);
-let env= 'development';
+let env = "development";
 let port = 3000;
 
-for(const arg of args){
-    if(arg.startsWith('--port=')){
-       port = Number(arg.split('=')[1]);
+for (const arg of args) {
+  if (arg.startsWith("--port=")) {
+    const parsedPort = Number(arg.split("=")[1]);
+
+    if (Number.isNaN(parsedPort) || parsedPort <= 0 || parsedPort > 65535) {
+      console.error("Invalid port. Use a number between 1 and 65535.");
+      process.exit(1);
     }
-    if(arg.startsWith('--env=')){
-        env = arg.split('=')[1];
-    }
+
+    port = parsedPort;
+  }
+
+  if (arg.startsWith("--env=")) {
+    env = arg.split("=")[1];
+  }
 }
 
-const server = http.createServer((req,res)=>{
-    res.writeHead(200,{'Content-Type':'text/plain'});
-    res.end('Hello World fnfnnffnn bdgdhhd');
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello World fnfnnffnn bdgdhhd");
 });
 
-server.listen(port,()=>{
-    console.log(`Server is running on port ${port} in ${env} mode`);
+server.listen(port, () => {
+  console.log(`Server is running on port ${port} in ${env} mode`);
 });
-
-
-
-
